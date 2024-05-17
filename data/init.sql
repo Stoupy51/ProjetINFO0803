43f040a58e23914ad2599e658ff8afd49e6d297e
Axelle <axelle.garrat@gmail.com>
2024-05-17 15:26:21 +0200

    test init.sql

---
+++ data/init.sql
@@ -0,0 +1,9 @@
+CREATE TABLE contacts (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    prenom VARCHAR(50),
+    nom VARCHAR(50),
+    email VARCHAR(100),
+    attributs VARCHAR(20)
+);
+
+INSERT INTO contacts (prenom, nom, email, attributs) VALUES ('John', 'Doe', 'john@example.com', '["employeur":"URCA","poste":"prof"]');
