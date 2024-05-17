1966d53a5bf913a9ae4c44bd1fce443e999c0f2b
Stoupy51 <stoupy51@gmail.com>
2024-05-17 14:36:19 +0200

    Code base

--- code_web_api/main.py
+++ code_web_api/main.py
@@ -1,2 +1,16 @@
 
+# Imports
+from flask import Flask
+
+# Création d'une instance de l'application Flask
+app = Flask(__name__)
+
+# Création d'un contexte en paramètre GET sur l'URL /
+@app.route("/", methods=["GET"])
+def get():
+	return "Hello World", 200
+
+# Exécution de l'application
+if __name__ == "__main__":
+	app.run("0.0.0.0")
 
