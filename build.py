8e4ffa961abd5ecd92082ac8b4b44a6c794369a8
Stoupy51 <stoupy51@gmail.com>
2024-05-21 11:08:47 +0200

    Web front + better build

--- build.py
+++ build.py
@@ -1,12 +1,9 @@
 
-commands = """
-docker build -t 803_database --file 803_database.dockerfile .
-docker build -t 803_web_api --file 803_web_api.dockerfile .
-docker build -t 803_web_front --file 803_web_front.dockerfile .
-docker compose up
-""".strip().split("\n")
-
 import os
-for command in commands:
-	os.system(command)
+import sys
+use_no_cache = " --no-cache" if "no" in sys.argv else ""
+os.system("docker build"+ use_no_cache + " -t 803_database --file 803_database.dockerfile .")
+os.system("docker build"+ use_no_cache + " -t 803_web_api --file 803_web_api.dockerfile .")
+os.system("docker build"+ use_no_cache + " -t 803_web_front --file 803_web_front.dockerfile .")
+os.system("docker compose up")
 
--- code_web_front/main.py
+++ code_web_front/main.py
@@ -1,14 +1,97 @@
 
-# Imports
-from flask import Flask
+# Vérification des arguments
+import sys
+if len(sys.argv) != 3:
+	print("Erreur d'utilisation, format :")
+	print(sys.executable, sys.argv[0], "<api_ip> <api_port>")
+	sys.exit(1)
+API_IP = sys.argv[1]
+API_PORT = sys.argv[2]
+
+# Import flask & constants
+import requests
+from flask import Flask, request, jsonify, Response
+BASE_URL = f"http://{API_IP}:{API_PORT}"
+API_GET = BASE_URL + "/api/get"
+API_ADD = BASE_URL + "/api/add"
+API_REMOVE = BASE_URL + "/api/remove"
 
 # Création d'une instance de l'application Flask
 app = Flask(__name__)
 
-# Création d'un contexte en paramètre GET sur l'URL /
+# Page principale: récupère toutes les informations
 @app.route("/", methods=["GET"])
-def get():
-	return "Hello World", 200
+def home():
+	contacts = []
+	try:
+		contacts = requests.get(API_GET).json()
+	except:
+		return "Erreur lors de la récupération des contacts", 500
+	
+	# Création de la page HTML
+	html = """
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Contacts</title>
+	</head>
+	<body>
+		<h1>Liste des contacts</h1>
+		<ul>
+"""
+	for contact in contacts:
+		remove_icon = "<a href='/remove?nom=" + contact["nom"] + "&prenom=" + contact["prenom"] + "'>❌</a>"
+		html += "\t\t\t<li>" + contact + remove_icon + "</li>\n"
+	html += """
+		</ul>
+		<h2>Ajouter un contact</h2>
+		<form method="POST" action="/add">
+			<label for="nom">Nom:</label>
+			<input type="text" id="nom" name="nom" required placeholder="Doe">
+			<label for="prenom">Prénom:</label>
+			<input type="text" id="prenom" name="prenom" required placeholder="John">
+			<label for="email">Email:</label>
+			<input type="email" id="email" name="email" required placeholder="john_doe@uwu.net">
+			<label for="attributs">Attributs:</label>
+			<input type="textarea" id="attributs" name="attributs" required placeholder='{"employeur": "URCA", "poste": "prof", "resp": "Directeur"}'>
+			<button type="submit">Ajouter</button>
+		</form>
+	</body>
+</html>
+"""
+	return html, 200
+
+# Ajout d'un contact
+@app.route("/add", methods=["POST"])
+def add_contact():
+
+	# Ajout du contact
+	try:
+		r = requests.post(API_ADD, data = request.form)
+		if r.status_code != 200:
+			return r.text, r.status_code
+	except:
+		return "Erreur lors de l'ajout du contact", 500
+	return home()
+
+# Suppression d'un contact
+@app.route("/remove", methods=["GET"])
+def remove_contact():
+	nom = request.args.get("nom")
+	prenom = request.args.get("prenom")
+	if not nom:
+		return "Nom manquant", 400
+	if not prenom:
+		return "Prénom manquant", 400
+
+	# Suppression du contact
+	try:
+		r = requests.post(API_REMOVE, data = {"nom": nom, "prenom": prenom})
+		if r.status_code != 200:
+			return r.text, r.status_code
+	except:
+		return "Erreur lors de la suppression du contact", 500
+	return home()
 
 # Exécution de l'application
 if __name__ == "__main__":
