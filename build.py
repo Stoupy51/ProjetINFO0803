a4da48121e338c0b49b0126e772ae099c4490a5e
Stoupy51 <stoupy51@gmail.com>
2024-05-21 10:32:50 +0200

    build.py

--- build.py
+++ build.py
@@ -1,6 +1,12 @@
 
+commands = """
 docker build -t 803_database --file 803_database.dockerfile .
 docker build -t 803_web_api --file 803_web_api.dockerfile .
 docker build -t 803_web_front --file 803_web_front.dockerfile .
 docker compose up
+""".strip().split("\n")
+
+import os
+for command in commands:
+	os.system(command)
 
